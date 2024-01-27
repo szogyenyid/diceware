83eab9697d5644a0e40e41c060ea6f60300f7705
szogyenyid <szogyenyid@pm.me>
2024-01-27 18:04:17 +0100

    add ability to set delimiter

--- diceware.sh
+++ diceware.sh
@@ -6,7 +6,7 @@ blnk=$(echo "$arg0" | sed 's/./ /g')
 usage_info()
 {
     echo "Usage: $arg0 [{-s|--size} size] [-e|--entropy] [-v|--verify]"
-    #echo "       $blnk [{-c|--ccc} ccc] [{-d|--ddd} ddd] \\"
+    echo "       $blnk [{-d|--delimiter} delimiter]"
 }
 
 help()
@@ -15,6 +15,7 @@ help()
     echo
     echo "  {-s|--size} size  -- The number of words in the passphrase (default: 6)"
     echo "  {-e|--entropy}    -- Show the entropy of the generated passphrase"
+    echo "  {-d|--delimiter}  -- Delimiter to use between words (default: space)"
     echo "  {-v|--verify}     -- Verifies if the present wordlist is the one provided by EFF"
     echo "  {-h|--help}       -- Print this help message and exit"
     exit 0
@@ -39,6 +40,11 @@ flags()
     while test $# -gt 0
     do
         case "$1" in
+        (-d|--delimiter)
+            shift
+            export DELIMITER="$1"
+            shift
+            OPTCOUNT=$(($OPTCOUNT + 2));;
         (-e|--entropy)
             shift
             export ENTROPY=1
@@ -61,6 +67,7 @@ flags()
 }
 
 SIZE=6
+DELIMITER=" "
 flags "$@"
 
 if [[ "$VERIFY" -eq "1" ]]; then
@@ -83,7 +90,12 @@ do
         CURRRAND=$(($((16#$(cat /dev/urandom | head -n 50 | od -x | cut -b 8-12,14-18 | xargs | sed 's/ //g' | cut -b 1-14)))%6 + 1))
         CURRNUM="${CURRNUM}${CURRRAND}"
     done
-    echo -ne $(grep ${CURRNUM} eff_large_wordlist.txt | cut -f 2) " "
+    WORD=$(grep ${CURRNUM} eff_large_wordlist.txt | cut -f 2)
+    if [ $i -eq $SIZE ]; then
+        echo -ne "$WORD"
+    else
+        echo -ne "$WORD$DELIMITER"
+    fi
 done
 echo
 
