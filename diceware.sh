a4a1993272b0c666d755c4683b5a495e7da4db5e
szogyenyid <szogyenyid@pm.me>
2024-01-28 10:19:26 +0100

    add quiet mode

--- diceware.sh
+++ diceware.sh
@@ -6,18 +6,19 @@ blnk=$(echo "$arg0" | sed 's/./ /g')
 usage_info()
 {
     echo "Usage: $arg0 [{-l|--length} length] [-e|--entropy] [-v|--verify]"
-    echo "       $blnk [{-d|--delimiter} delimiter]"
+    echo "       $blnk [{-d|--delimiter} delimiter] [-q|--quiet]"
 }
 
 help()
 {
     usage_info
     echo
-    echo "  {-l|--length} length  -- The number of words in the passphrase (default: 6)"
-    echo "  {-e|--entropy}        -- Show the entropy of the generated passphrase"
-    echo "  {-d|--delimiter}      -- Delimiter to use between words (default: space)"
-    echo "  {-v|--verify}         -- Verifies if the present wordlist is the one provided by EFF"
-    echo "  {-h|--help}           -- Print this help message and exit"
+    echo "  {-l|--length} length        -- The number of words in the passphrase (default: 6)"
+    echo "  {-e|--entropy}              -- Show the entropy of the generated passphrase"
+    echo "  {-d|--delimiter} delimiter  -- Delimiter to use between words (default: space)"
+    echo "  {-q|--quiet}                -- Only print the passphrase"
+    echo "  {-v|--verify}               -- Verifies if the present wordlist is the one provided by EFF"
+    echo "  {-h|--help}                 -- Print this help message and exit"
     exit 0
 }
 
@@ -57,6 +58,10 @@ flags()
             export LENGTH="$1"
             shift
             OPTCOUNT=$(($OPTCOUNT + 2));;
+        (-q|--quiet)
+            shift
+            export QUIET=1
+            OPTCOUNT=$(($OPTCOUNT + 1));;
         (-v|--verify)
             shift
             export VERIFY=1
@@ -72,25 +77,37 @@ DELIMITER=" "
 flags "$@"
 
 if ! test -f $BASEDIR/eff_large_wordlist.txt; then
-    echo "Wordlist not found. Trying to download..."
-    $(wget -P $BASEDIR https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt)
+    if [[ "$QUIET" -eq "1" ]]; then
+        $(wget -q -P $BASEDIR https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt)
+    else
+        echo "Wordlist not found. Trying to download..."
+        $(wget -P $BASEDIR https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt)
+    fi
     VERIFY=1
 fi
 
 if [[ "$VERIFY" -eq "1" ]]; then
-    echo "Verifying fingerprints..."
+    if [[ "$QUIET" -ne "1" ]]; then
+        echo "Verifying fingerprints..."
+    fi
     EFFFINGERPRINT=$(wget -qO- https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt | openssl dgst -sha256 | cut -d " " -f 2)
     LISTFINGERPRINT=$(cat $BASEDIR/eff_large_wordlist.txt | openssl dgst -sha256 | cut -d " " -f 2)
     if [[ "$LISTFINGERPRINT" != "$EFFFINGERPRINT" ]]; then
-        echo "Your dictionary differs from the one provided by EFF. Aborting"
+        if [[ "$QUIET" -ne "1" ]]; then
+            echo "Your dictionary differs from the one provided by EFF. Aborting"
+        fi
         exit 1
     fi
-    echo "Verification successful"
-    echo
+    if [[ "$QUIET" -ne "1" ]]; then
+        echo "Verification successful"
+        echo
+    fi
 fi
 
 if [[ "$LENGTH" -lt 6 ]]; then
-    echo "Warning! It is advised to use at least 6 words for a passphrase."
+    if [[ "$QUIET" -ne "1" ]]; then
+        echo "Warning! It is advised to use at least 6 words for a passphrase."
+    fi
 fi
 
 for i in $(seq 1 $LENGTH);
@@ -111,7 +128,9 @@ done
 echo
 
 if [[ "$ENTROPY" -eq "1" ]]; then
-    echo
-    echo "Entropy: ~$(($LENGTH*323/25)) bits"
+    if [[ "$QUIET" -ne "1" ]]; then
+        echo
+        echo "Entropy: ~$(($LENGTH*323/25)) bits"
+    fi
 fi
 exit 0
